9ca9abb5722800b7cb1e09085c4b3e30c9ff7a98
tprax <tprax01@gmail.com>
2018-09-10 15:49:38 -0600

    updated board controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,49 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only:[:show, :edit, :update, :destroy]
+
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(params_board)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @board.update(params_board)
+      redirect_to boards_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path
+  end
+
+  private
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+  
+  def params_board
+    params.require(:board).permit(:message, :id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'boards#index'
   resources :boards do
     resources :lists do 
       resources :tasks
